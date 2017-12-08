f246eab7fb90319759ded89d60a6ae3ff209c2a6
Omar Abd-Al-Fattah <omarspps5@gmail.com>
2017-12-08 10:38:47 +0200

    Update Task 1: Students database.cpp
    1 -> Done
    2-> Done
    3-> Done
    4, 5 -> Working on it (search student by first name and ID)
    6 -> Exit the programe , not a big deal.

--- Task 1: Students database.cpp
+++ Task 1: Students database.cpp
@@ -1,5 +1,7 @@
 /** The Code For this Task Goes Here, Please add some documentations and Comments to make the reading easier **/ 
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
 
 const int MAX_NAME=14; // Assume a name contains at most 14 characters
@@ -7,7 +9,7 @@ enum Gender{ MALE = 'm', FEMALE = 'f'};
 string first_name;    //global vars
 string last_name;
 double GPA;
-int ID;
+int ID, n, temp = 0;
 
 
 struct Date{
@@ -42,7 +44,7 @@ void InputStudent(Student *s){
 
 void OutputStudent(Student *s){
     cout<<s->ID<<endl;
-    cout<<s->first_name<<"adasd"<<endl;
+    cout<<s->first_name<<endl;
     cout<<s->last_name<<endl;
     cout<<s->birth_date.year<<endl;
     cout<<s->birth_date.month<<endl;
@@ -50,12 +52,47 @@ void OutputStudent(Student *s){
     cout<<s->GPA<<endl;
 }
 
+void InputAllStudent(Student *s ,int n){
+    bool LessThan(s);
+    bool GPAGreater(s);
+
+}
+void OutputAllStudent(Student *s ,int n){
+    for(int i = 0 ; i < n ; i++){
+        OutputStudent(s);
+    }
+}
+bool LessThan(Student *s){
+    for(int i = 0 ; i < n ; i++){
+        InputStudent(s);
+
+        if(temp < s->ID){
+            return 1;
+        } else return 0;
+
+        s->ID = temp;
+    }
+}
+
+bool GPAGreater (Student *s){
+    for(int i = 0 ; i < n ; i++){
+        InputStudent(s);
+
+        if(temp > s->GPA){
+            return 1;
+        } else return 0;
+    }
+}
+
+void SortStudents(Student* s, int n, bool (*LessThan)(Student*,Student*)){
+
+}
 
 int main()
 {
     Student* s;
-    InputStudent(s);
-    OutputStudent(s);
+    InputAllStudent(s, n);
+    OutputAllStudent(s, n);
     return 0;
 }
 
